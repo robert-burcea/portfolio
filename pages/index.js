8c229fe2cc35a07d7b5525ffcbf0dc9456d8adf8
rburcea <robert.burcea@gmail.com>
2022-09-04 15:00:01 +0300

    change message

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1 className="mx-auto w-[1240px] text-center bg-gray-200">MY FIRST NEXT APP</h1>
+      <h1 className="mx-auto w-[1240px] text-center bg-gray-200">Here will be Robert Burcea's portfolio website</h1>
     </div>
   )
 }
